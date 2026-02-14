100c14fa7da03152c62f9a73d83d9fe59cfb49b1
vmassonniere <vmassonniere@icebergdatalab.com>
2026-02-14 20:38:09 +0100

    initial commit

---
+++ hello_cargo/target/release/deps/hello_cargo-008ee8bd66c1cd6a.d
@@ -0,0 +1,5 @@
+/home/user/Documents/dev-perso/rust/hello_cargo/target/release/deps/hello_cargo-008ee8bd66c1cd6a.d: src/main.rs
+
+/home/user/Documents/dev-perso/rust/hello_cargo/target/release/deps/hello_cargo-008ee8bd66c1cd6a: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/user/Documents/dev-perso/rust/hello_cargo/target/release/hello_cargo: /home/user/Documents/dev-perso/rust/hello_cargo/src/main.rs
